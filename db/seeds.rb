7de9de495b960b244124a081c21371beeae390a1
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-09-13 18:19:59 +0200

    models creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+onigiri = Recipe.new(title: "onigiri")
+onigiri.save
+
+rice = Ingredient.new(name: "rice")
+rice.save
+
+umeboshi = Ingredient.new(name: "umeboshi")
+umeboshi.save
+
+vegan = Tag.new(name: "vegan")
+vegan.save
+
+oishi = Tag.new(name: "oishi")
+oishi.save
+
+measure1 = Measure.new(description: "1 boulette de")
+measure1.ingredient = rice
+measure1.recipe = onigiri
+measure1.save
+
+measure2 = Measure.new(description: "1 boulette de")
+measure2.ingredient = umeboshi
+measure2.recipe = onigiri
+measure2.save
+
+couteau = Tool.new(name: "couteau")
+couteau.save
+
+hashi = Tool.new(name: "hashi")
+hashi.save
+
+onigiri.tools << couteau
+onigiri.tools << hashi
+
+onigiri.tags << vegan
+onigiri.tags << oishi
+onigiri.save
+
