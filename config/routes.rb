f820435991bd2176a7660d705f8263c42af79dfc
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-09-18 14:04:24 +0200

    adding recipes routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
   devise_for :users
   root to: 'pages#home'
+
+  resources :recipes, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
