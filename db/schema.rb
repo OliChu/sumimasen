6c106bc5d3f4caea22dcfcd37c5f360a1019b875
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-09-21 21:30:31 +0200

    rollback measures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920202238) do
+ActiveRecord::Schema.define(version: 20170918142144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,13 +38,11 @@ ActiveRecord::Schema.define(version: 20170920202238) do
   end
 
   create_table "measures", force: :cascade do |t|
-    t.string   "text_1"
+    t.string   "description"
     t.integer  "ingredient_id"
     t.integer  "recipe_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.string   "text_2"
-    t.integer  "quantity"
     t.index ["ingredient_id"], name: "index_measures_on_ingredient_id", using: :btree
     t.index ["recipe_id"], name: "index_measures_on_recipe_id", using: :btree
   end
